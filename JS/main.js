a7743bfad0d434cf66a18a827b4fd72bce1b18b1
joohee <2170095@ewhain.net>
2023-02-20 20:02:34 +0900

    hover, view 이동 기능 추가

--- JS/main.js
+++ JS/main.js
@@ -132,3 +132,48 @@ var weekDate = document.getElementsByClassName("weekDate");
 for(var j=0; j<7; j++){
   weekDate[j].innerText = thisWeek[j+1];
 }
+
+var circle = document.getElementsByClassName("circle");
+
+/*
+circle 클릭하면 view 이동하는 기능
+
+class명 'box'로 통일 -> box 자식 노드로 접근 & 반복문으로 구현하려 했으나 오류
+
+var box = document.getElementsByClassName("box");
+for(var k=0; k<7; k++){
+  circle[k].addEventListener("click", function(){
+    box[k].scrollIntoView({behavior: "smooth"});
+  })
+}
+*/
+
+//일단 각각 ID로 접근하여 구현
+circle[0].addEventListener("click", function(){
+  var Mon = document.getElementById("Mon"); 
+  Mon.scrollIntoView({behavior: "smooth"});
+})
+circle[1].addEventListener("click", function(){
+  var Tue = document.getElementById("Tue"); 
+  Tue.scrollIntoView({behavior: "smooth"});
+})
+circle[2].addEventListener("click", function(){
+  var Wed = document.getElementById("Wed"); 
+  Wed.scrollIntoView({behavior: "smooth"});
+})
+circle[3].addEventListener("click", function(){
+  var Thur = document.getElementById("Thur"); 
+  Thur.scrollIntoView({behavior: "smooth"});
+})
+circle[4].addEventListener("click", function(){
+  var Fri = document.getElementById("Fri"); 
+  Fri.scrollIntoView({behavior: "smooth"});
+})
+circle[5].addEventListener("click", function(){
+  var Sat = document.getElementById("Sat"); 
+  Sat.scrollIntoView({behavior: "smooth"});
+})
+circle[6].addEventListener("click", function(){
+  var Sun = document.getElementById("Sun"); 
+  Sun.scrollIntoView({behavior: "smooth"});
+})
