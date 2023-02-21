a437b77ebc83252b4aa125d825ee82de9c4199b1
LJH <049494@ewhain.net>
2023-02-21 16:04:07 +0900

    일정 체크 기능 오류 수정

--- JS/main.js
+++ JS/main.js
@@ -23,16 +23,22 @@ addToDo.addEventListener("click", function () {
   var span2 = document.createElement("span");
   var span3 = document.createElement("span");
   var span4 = document.createElement("span");
-  var list = document.createElement("h4");
-  
+  var check_btn = document.createElement("input");
+  var list = document.createElement("span");
+  var br = document.createElement("br");
+
+  check_btn.setAttribute("type", "checkbox");
+
   var modify = document.createElement("input");
   modify.type = "button";
   modify.value = "수정";
-  modify.style = "float:right; border:0px; background-color: transparent; font-size:13px;";
+  modify.style =
+    "float:right; border:0px; background-color: transparent; font-size:13px;";
   var del = document.createElement("input");
   del.type = "button";
   del.value = "삭제";
-  del.style = "float:right; border:0px; background-color: transparent; font-size:13px;";
+  del.style =
+    "float:right; border:0px; background-color: transparent; font-size:13px;";
 
   if (!inputBox.value) alert("내용을 입력해 주세요!");
   if (!date.value) alert("요일을 선택해주세요!");
@@ -45,12 +51,12 @@ addToDo.addEventListener("click", function () {
     span4.innerText = minute.value || "0";
     list.innerText = inputBox.value;
 
-    toDoList.append(
-      div
-    )
+    toDoList.append(div);
 
     div.append(
+      check_btn,
       list,
+      br,
       del,
       modify,
       "예정 시간 : ",
@@ -73,42 +79,34 @@ addToDo.addEventListener("click", function () {
     condition.style.display = "none";
   }
 
-  list.addEventListener("click", function () {
-    list.style.textDecoration = "line-through";
+  check_btn.addEventListener("click", function () {
+    list.style.textDecoration = check_btn.checked ? "line-through" : "none";
   });
 
-  modify.addEventListener("click", function() {
-    list.innerText="";
+  modify.addEventListener("click", function () {
+    list.innerText = "";
     var modInput = document.createElement("input");
     modInput.type = "text";
     modInput.id = "modInput";
     var modInBtn = document.createElement("input");
     modInBtn.type = "button";
     modInBtn.value = "수정";
-    
-    list.append(
-    modInput,
-    modInBtn
-    );
 
-    modInBtn.addEventListener("click", function(){
+    list.append(modInput, modInBtn);
+
+    modInBtn.addEventListener("click", function () {
       var modIn = document.getElementById("modInput");
       list.innerText = modIn.value;
-      /*
-      밑줄 지우기 기능 오류
-      list.style.textDecoration = "none";
-      */
-    });    
-  })
+    });
+  });
 
-  del.addEventListener("click", function(){
-    if(!confirm('정말로 삭제하시겠습니까?')){
+  del.addEventListener("click", function () {
+    if (!confirm("정말로 삭제하시겠습니까?")) {
       return false;
-    }
-    else{
+    } else {
       div.remove();
     }
-  })
+  });
 });
 
 //이번주 날짜 표시
@@ -119,18 +117,18 @@ var theDate = currentDate.getDate();
 var theDay = currentDate.getDay();
 var thisWeek = [];
 
-for(var i=1; i<8; i++) {
+for (var i = 1; i < 8; i++) {
   var resultDay = new Date(theYear, theMonth, theDate + (i - theDay));
   var mm = Number(resultDay.getMonth()) + 1;
   var dd = resultDay.getDate();
 
-  thisWeek[i] = mm + '/' + dd;
+  thisWeek[i] = mm + "/" + dd;
 }
 
 var weekDate = document.getElementsByClassName("weekDate");
 
-for(var j=0; j<7; j++){
-  weekDate[j].innerText = thisWeek[j+1];
+for (var j = 0; j < 7; j++) {
+  weekDate[j].innerText = thisWeek[j + 1];
 }
 
 // circle 클릭하면 view 이동하는 기능
@@ -158,31 +156,31 @@ for(var k=0; k<7; k++){
 */
 
 //일단 각각 ID로 접근하여 구현
-circle[0].addEventListener("click", function(){
-  var Mon = document.getElementById("Mon"); 
-  Mon.scrollIntoView({behavior: "smooth"});
-})
-circle[1].addEventListener("click", function(){
-  var Tue = document.getElementById("Tue"); 
-  Tue.scrollIntoView({behavior: "smooth"});
-})
-circle[2].addEventListener("click", function(){
-  var Wed = document.getElementById("Wed"); 
-  Wed.scrollIntoView({behavior: "smooth"});
-})
-circle[3].addEventListener("click", function(){
-  var Thur = document.getElementById("Thur"); 
-  Thur.scrollIntoView({behavior: "smooth"});
-})
-circle[4].addEventListener("click", function(){
-  var Fri = document.getElementById("Fri"); 
-  Fri.scrollIntoView({behavior: "smooth"});
-})
-circle[5].addEventListener("click", function(){
-  var Sat = document.getElementById("Sat"); 
-  Sat.scrollIntoView({behavior: "smooth"});
-})
-circle[6].addEventListener("click", function(){
-  var Sun = document.getElementById("Sun"); 
-  Sun.scrollIntoView({behavior: "smooth"});
-})
+circle[0].addEventListener("click", function () {
+  var Mon = document.getElementById("Mon");
+  Mon.scrollIntoView({ behavior: "smooth" });
+});
+circle[1].addEventListener("click", function () {
+  var Tue = document.getElementById("Tue");
+  Tue.scrollIntoView({ behavior: "smooth" });
+});
+circle[2].addEventListener("click", function () {
+  var Wed = document.getElementById("Wed");
+  Wed.scrollIntoView({ behavior: "smooth" });
+});
+circle[3].addEventListener("click", function () {
+  var Thur = document.getElementById("Thur");
+  Thur.scrollIntoView({ behavior: "smooth" });
+});
+circle[4].addEventListener("click", function () {
+  var Fri = document.getElementById("Fri");
+  Fri.scrollIntoView({ behavior: "smooth" });
+});
+circle[5].addEventListener("click", function () {
+  var Sat = document.getElementById("Sat");
+  Sat.scrollIntoView({ behavior: "smooth" });
+});
+circle[6].addEventListener("click", function () {
+  var Sun = document.getElementById("Sun");
+  Sun.scrollIntoView({ behavior: "smooth" });
+});
